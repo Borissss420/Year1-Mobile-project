13060411178226b5d025e9ce2d5def00e3188db2
Boris Hoi <Boris.Hoi@metropolia.fi>
2020-11-26 17:42:01 +0800

    Added Calendar

--- app/src/main/java/fi/Team4/timetrackerapp/MainActivity.java
+++ app/src/main/java/fi/Team4/timetrackerapp/MainActivity.java
@@ -2,7 +2,14 @@ package fi.Team4.timetrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView time = findViewById(R.id.tvCurrentTime);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat dateAndTime = new SimpleDateFormat("'Date:' EEE, dd.MMM.yyyy \n'Zone:' z");
+        String currentDateAndTime = dateAndTime.format(new Date());
+        time.setText(currentDateAndTime);
+
+        Button scheduleButton = findViewById(R.id.btnSchedule);
+        scheduleButton.setOnClickListener(v -> {
+            Intent intent1 = new Intent(MainActivity.this, ScheduleActivity.class);
+            startActivity(intent1);
+        });
+
+        Button statisticsButton = findViewById(R.id.btnStatistics);
+        statisticsButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, StatisticActivity.class));
+        });
     }
 }
--- app/src/main/java/fi/Team4/timetrackerapp/ScheduleActivity.java
+++ app/src/main/java/fi/Team4/timetrackerapp/ScheduleActivity.java
@@ -1,8 +1,11 @@
 package fi.Team4.timetrackerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        CalendarView calendar = findViewById(R.id.calendarView);
+        TextView selectedDate  = findViewById(R.id.tvSelectedDate);
+
+        calendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            String date = dayOfMonth + "/" + (month + 1) + "/" + year ;
+            selectedDate.setText(date);
+        });
     }
 }
